0e64077380d9b82c166ded855ecf8c0eb7cae2a5
flootah <ed.saenz@protonmail.ch>
2021-02-08 19:18:33 -0800

    more removal and basic setup

---
+++ js/main.js
@@ -0,0 +1,6 @@
+var input = document.getElementById("response");    // get input element from html
+input.onclick = checkAnswer();                      // call checkAnswer when input is submitted
+
+function checkAnswer(answer) {
+    
+}
